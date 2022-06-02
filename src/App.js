153b2198842a0f56b4869b51d2f7d288563bcec7
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-06-01 20:05:27 -0400

    imported homepage, form, axios, state, effect, route, and switch to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./Components/Homepage";
+import Form from "./Components/OrderForm";
+
 
 const App = () => {
   return (
