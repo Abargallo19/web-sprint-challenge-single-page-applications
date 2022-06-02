5385cf2cb8e144748cdb3fdb996957df0fb5913b
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-06-01 23:27:00 -0400

    inputs almost finished, debugging disabled error

--- src/Components/OrderForm.js
+++ src/Components/OrderForm.js
@@ -21,31 +21,56 @@ const onSubmit = evt => {
 return ( <form id="pizza-form" onSubmit={onSubmit}>
     <section className="form-container">
         <h1>Tell Us What You Like</h1>
+        <p>{errors.name}</p>
+        <p>{errors.size}</p>
+
         <div className="ordername">
             <h3>What's the Name for the Order?</h3>
             <label>Name
                 <input
-                
+                name='name'
+                value={values.name}
+                onChange={onChange}
+                type='text'
                 />
             </label>
         </div>
 
             <label>Size
-                <input />
+               <select
+               onChange={onChange}
+               value={values.size}
+               name='size'
+               >
+                   <option value=''>--Please Select a Size--</option>
+                   <option value='personal'>Personal Pan</option>
+                   <option value='medium'>Medium</option>
+                   <option value='large'>Large</option>
+               </select>
             </label>
 
-
-            <label>Toppings
-                <input />
+        <h3>Toppings</h3>
+            <label>Pepperoni
+                <input
+                name='pepperoni'
+                type="checkbox"
+                onChange={onChange}
+                checked={values.pepperoni}
+                />
             </label>
 
 
             <label>Special Instructions
-                <input />
+                <input 
+                name='special'
+                value={values.special}
+                onChange={onChange}
+                type='text'
+                />
             </label>
         
 
-
+<input disabled={disabled} type = 'submit' value = 'Add to Order'/>
 
 
 
