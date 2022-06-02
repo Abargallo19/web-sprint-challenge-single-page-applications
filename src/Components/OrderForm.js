9c5c0788c0a7c6468c4e336b1c524e4fd460fa71
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-06-01 22:52:50 -0400

    built an onchange handler inside form

--- src/Components/OrderForm.js
+++ src/Components/OrderForm.js
@@ -1,7 +1,18 @@
 import { React } from 'react';
 
 
-export default function OrderForm() {
+export default function OrderForm(props) {
+
+const { values, errors, disabled, change, submit } = props;
+
+const onChange = evt => {
+    const { name, value, checked, type } = evt.target;
+    const useValue = type === "checkbox" ? checked : value;
+    change(name, useValue);
+}
+
+
+
 
 
 return ( <form id="pizza-form">
